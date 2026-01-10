0b1000e01e98e6f98047f1b5e1ca00d18aefda68
Glib <omelniskij@gmail.com>
2026-01-10 16:57:50 +0200

    Player move

---
+++ SaintTest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private float _moveSpeed;
+    
+    private void FixedUpdate()
+    {
+        _rigidBody.velocity = new Vector3(_joystick.Horizontal, 0, _joystick.Vertical) * _moveSpeed;
+
+        if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
+        {
+            _player.rotation = Quaternion.LookRotation(_rigidBody.velocity);
+        }
+    }
+}
